bc3dc411a4790d48999839321a31b086853fb9d0
Loneprog <wolfdon73@gmail.com>
2024-12-21 13:22:16 +0000

    start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/*import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";*/
+import "./App.css";
+
+const App = () => {
+	console.log("Hello from vite+react")
+	return (
+		<div>
+			<h1>Hello World!</h1>
+		</div>
+	);
+};
+
+export default App;
