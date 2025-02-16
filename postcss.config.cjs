199d608bc2676727c39a43a2807e2af9eac85407
LoneProg <131486112+LoneProg@users.noreply.github.com>
2025-02-16 16:53:16 +0000

    Add Tailwind CSS integration, refactor Navbar and App components, and create Hero component

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,4 +1,3 @@
-// PostCSS configuration file
 module.exports = {
   plugins: {
     '@tailwindcss/postcss': {},
---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import HeroImage from '../assets/Profile-mask.jpg'
+
+const Hero = () => {
+  return (
+    <div className='bg-black text-white text-center py-16 '>
+        <img src={HeroImage} alt="ProfileImage" className='mx-auto mb-8 h-48 rounded-full object-cover transform transition-transform duration-300 hover:scale-105' />
+        <h1 className='text-4xl font-bold'>
+            Im {''}
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-400'>Haze Crypt</span>
+            ,a full stack web developer 
+        </h1>
+        <p>
+            I specialize in building fast, responsive, and accessible websites
+        </p>
+        <div>
+            <button>Contact me</button>
+            <button>View Resume</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
