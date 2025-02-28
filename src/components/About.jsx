dc8f28614f83ed17a8b76b35c58c48d928291056
LoneProg <131486112+LoneProg@users.noreply.github.com>
2025-02-28 11:21:39 +0000

    Update About component with new skill labels and add Service component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,20 +19,34 @@ const About = () => {
                             <div className="flex items-center">
                                 <label htmlFor="htmlandcss" className="w-2/12">HTML & CSS</label>
                                 <div className="grow bg-gray-800 rounded-full h-2.5 transform transition-transform duration-300 hover:scale-105">
-                                    <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full w-10/12"></div>
+                                    <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full w-11/12"></div>
                                 </div>
                             </div>
                             <div className="flex items-center">
-                                <label htmlFor="htmlandcss" className="w-2/12">HTML & CSS</label>
+                                <label htmlFor="nodejs" className="w-2/12">NODE JS</label>
                                 <div className="grow bg-gray-800 rounded-full h-2.5 transform transition-transform duration-300 hover:scale-105">
-                                    <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full w-10/12"></div>
+                                    <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full w-9/12"></div>
                                 </div>
                             </div>
                             <div className="flex items-center">
-                                <label htmlFor="htmlandcss" className="w-2/12">HTML & CSS</label>
+                                <label htmlFor="react" className="w-2/12">REACT</label>
                                 <div className="grow bg-gray-800 rounded-full h-2.5 transform transition-transform duration-300 hover:scale-105">
-                                    <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full w-10/12"></div>
+                                    <div className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full w-8/12"></div>
                                 </div>
+                            </div>
+                        </div>
+                        <div className="mt-12 flex justify-between text-center ">
+                            <div>
+                                <h3 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">2+</h3>
+                                <p>Years Experience</p>
+                            </div>
+                            <div>
+                                <h3 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">10+</h3>
+                                <p>Projects Completed</p>
+                            </div>
+                            <div>
+                                <h3 className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">10+</h3>
+                                <p>Happy Clients</p>
                             </div>
                         </div>
                     </div>
---
+++ src/components/Service.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Service = () => {
+    return (
+        <div className="bg-black text-white py-20" id="about">
+            <div className="container mx-auto px-8 md:px-16 lg:px-24">
+                <h2>About me</h2>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
